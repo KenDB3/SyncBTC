64e4bb1daddedbb36c015c5ff0f2c6838a4bd131
Kenny DiBattista <KenDB3@users.noreply.github.com>
2016-09-02 15:58:54 -0400

    The terminal size can get bigger and get more data
    This is where I realized I could make the data get bigger to fill 
    just about any terminal size. We have a lower limit, but really no
    particular upper limit.

--- bitcoin.js
+++ bitcoin.js
@@ -5,6 +5,7 @@ function bitcoinprice() {
 		var totalHeight = console.screen_rows;
 		var appHeight = (console.screen_rows - 6); //minus 3 top, minus 3 bottom
 		var yAxisHeight = (appHeight - 2); //minus 2 more for max/min data points
+		var totalLength = console.screen_columns;
 		var appLength = (console.screen_columns - 1);
 		var xAxisLength = (console.screen_columns - 4); //minus 3 for numbers on left, minus 1 for border on right 
 		var gray = "\1n\001w"; //Synchronet Ctrl-A Code for Normal White (which looks gray)
@@ -27,9 +28,10 @@ function bitcoinprice() {
 		write(magenta + "\r\nPolling Live and Historical Bitcoin Prices... Please be patient.");
 		var btcHeader = blue + "\r\n  Current Exchange Rate: \r\n" + 
 			darkblue + " \325\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315 Bitcoin Price Chart (USD) \315\315";
-		var btcFooter = darkmagenta + " \324\315oldest\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315newest\315\270\r\n" 
-			+ darkmagenta + "  syncBTC:" + blue + "KenDB3  " + darkmagenta + "Data:" + blue + "Coinbase.com  " + darkmagenta + "Original Concept:" + blue + "BCR / BitSunrise.com " + darkmagenta + "\315\315\315\276\r\n";
-			
+		var btcFooter = darkmagenta + " \324\315oldest\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\315\r\n" 
+			+ darkmagenta + "  syncBTC:" + blue + "KenDB3  " + darkmagenta + "Data:" + blue + "Coinbase.com  " + darkmagenta + "Original Concept:" + blue + "BCR / BitSunrise.com " + darkmagenta + "\315\315\315\r\n";
+		var btcFooterEnd1 = "\270";
+		var btcFooterEnd2 = "\276";
 		var btcxAxis = " \263";
         var reqExchangeRate = new HTTPRequest();
 		var reqHistoricPriceDays = new HTTPRequest();
@@ -47,7 +49,7 @@ function bitcoinprice() {
 		// Parse the JSON responses.
 		var jsonExchangeRate = JSON.parse(exchangeRate);
 		var jsonHistoricPriceDays = JSON.parse(historicPriceDays);
-		// (Obviously stolen from syncWXremix... this sits here a while) Check if the JSON is properly formatted. The "data" should wrap the entire object.
+		// Check if the JSON is properly formatted. The "data" should wrap the entire object.
 		/* make some error checking at some point
 		if (jsonExchangeRate.hasOwnProperty("data") ) {
 			// Check if the response contains an error message. If so, log the error and exit.
@@ -71,6 +73,7 @@ function bitcoinprice() {
 		var rateEUR = jsonExchangeRate.data.rates.EUR;
 		var l = jsonHistoricPriceDays.data.prices.length;
 		
+		//This got messy and difficult, so creating an intermediate array to hold the data made things easier
 		var Intermezzo = [];
 		var j = 0;
 		while (j < l) {
@@ -79,6 +82,8 @@ function bitcoinprice() {
 			j++;
 		}
 		
+		//This is the real array we want, the values of the data are too high (obviously)
+		//So, we are normalizing the data to make it fit on a terminal window (typically 24/25 rows, but hopefully we can make it variable)
 		var btcArray = [];
 		var btcMax = Math.max.apply(null, Intermezzo);
 		var btcMaxWhole = Math.round(btcMax);
@@ -92,13 +97,28 @@ function bitcoinprice() {
 			btcArray.push(wholeNumNormData);
 			i++;
 		}
+		var normBtcMax = Math.max.apply(null, btcArray);
+		var normBtcMin = Math.min.apply(null, btcArray);
 		
-		//Draw the header, footer, chart axes, etc
+		//Draw the header, footer, chart x and y axis, etc
 		console.clear();
+		//The header is going to be at least 79 characters long.
 		console.gotoxy(1,1);
 		console.putmsg(btcHeader);
+		//If the user's terminal window is longer, we extend it
+		if (appLength > 79) {
+			console.gotoxy(79,3);
+			write (darkblue);
+			var headerExt = 78;
+			while (headerExt < appLength) {
+				write ("\315");
+				headerExt++;
+			}
+		}
 		console.gotoxy(1,4);
+		//Write the Max value of the y-axis
 		console.putmsg(darkblue + btcMaxWhole);
+		//Draw a color coded y-axis
 		var k = 0;
 		while (k < (yAxisHeight + 1)) {
 			console.gotoxy(1,(k + 5));
@@ -131,43 +151,85 @@ function bitcoinprice() {
 			k++;
 		}
 		console.gotoxy(1,(totalHeight - 3));
+		//Write the Min value of the y-axis
 		console.putmsg(darkmagenta + btcMinWhole);
 		console.gotoxy(1,(totalHeight - 2));
+		//Write the footer, which is at least 79 characters long
 		console.putmsg(btcFooter);
+		//If the user's terminal window is longer, we extend it
+		if (appLength > 79) {
+			console.gotoxy(79,(totalHeight - 2));
+			write (darkmagenta);
+			var footerExt = 79;
+			while (footerExt < appLength) {
+				write ("\315");
+				footerExt++;
+			}
+			write(btcFooterEnd1);
+			console.gotoxy((totalLength - 9),(totalHeight - 2));
+			write (darkmagenta);
+			write ("newest");
+		} else {
+			console.gotoxy(79,(totalHeight - 2));
+			write (darkmagenta);
+			write(btcFooterEnd1);
+			console.gotoxy((totalLength - 9),(totalHeight - 2));
+			write (darkmagenta);
+			write ("newest");
+		}
+		if (appLength > 79) {
+			console.gotoxy(79,(totalHeight - 1));
+			write (darkmagenta);
+			var footerExt = 79;
+			while (footerExt < appLength) {
+				write ("\315");
+				footerExt++;
+			}
+			write(btcFooterEnd2);
+			console.gotoxy(1,(totalHeight));
+		} else {
+			console.gotoxy(79,(totalHeight - 1));
+			write (darkmagenta);
+			write(btcFooterEnd2);
+			console.gotoxy(1,(totalHeight));
+		}
+		//this next crlf bumps everything UP one line on the terminal, and therefore affects where we put the data. 
+		//trying to decide if this is good or bad or neutral
 		console.crlf();
 		console.gotoxy(26,1);
 		//Draw current Exchange Rate at the top in USD, EUR, and GBP
 		console.putmsg(darkcyan + "\044" + rateUSD + " USD/BTC" + " - \356" + rateEUR + " EUR/BTC" + " - \234" + rateGBP + " GPB/BTC");
 		
-		//Now to plot the data
+		//Now to plot the normalized data
 		//When you plot these data points they are all backwards, you need to FLIP everything
-		//start plotting X from the right, because most recent data is at the front of the JSON DB data
-		//Y-axis (after the flip) needs a +5 to get above the footer area
+		//start plotting X from the right, because most recent data is at the front of the JSON DB data (starting from 0) 
+		//Y-axis (after the flip) needs a modifier to get above the footer area (tried - 5, but sometimes it doesn't work, something is wrong...)
+		var normYAxis = 4 - normBtcMin;
 		var plot = 0;
 		while (plot < xAxisLength) {
-			var flip = appHeight - btcArray[plot];
-			console.gotoxy((appLength - plot),flip + 5);
-			if ((flip / yAxisHeight) <= ((1/12) * 1)) {
+			var flip = totalHeight - btcArray[plot];
+			console.gotoxy((appLength - plot),(flip - normYAxis));
+			if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 1)) {
 				write(darkblue);
-			} else if ((flip / yAxisHeight) <= ((1/12) * 2)) {
+			} else if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 2)) {
 				write(blue);
-			} else if ((flip / yAxisHeight) <= ((1/12) * 3)) {
+			} else if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 3)) {
 				write(darkcyan);
-			} else if ((flip / yAxisHeight) <= ((1/12) * 4)) {
+			} else if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 4)) {
 				write(cyan);
-			} else if ((flip / yAxisHeight) <= ((1/12) * 5)) {
+			} else if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 5)) {
 				write(darkgreen);
-			} else if ((flip / yAxisHeight) <= ((1/12) * 6)) {
+			} else if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 6)) {
 				write(green);
-			} else if ((flip / yAxisHeight) <= ((1/12) * 7)) {
+			} else if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 7)) {
 				write(yellow);
-			} else if ((flip / yAxisHeight) <= ((1/12) * 8)) {
+			} else if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 8)) {
 				write(darkyellow);
-			} else if ((flip / yAxisHeight) <= ((1/12) * 9)) {
+			} else if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 9)) {
 				write(red);
-			} else if ((flip / yAxisHeight) <= ((1/12) * 10)) {
+			} else if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 10)) {
 				write(darkred);
-			} else if ((flip / yAxisHeight) <= ((1/12) * 11)) {
+			} else if (((flip - normYAxis) / yAxisHeight) <= ((1/12) * 11)) {
 				write(magenta);
 			} else {
 				write(darkmagenta);
@@ -175,6 +237,10 @@ function bitcoinprice() {
 			write("\052");
 			plot++;
 		}
+		console.gotoxy(5,5);
+		write(normBtcMax);
+		console.gotoxy(5,6);
+		write(normBtcMin);
 		console.gotoxy(1,(totalHeight - 1));
 }
 
