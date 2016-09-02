af6843f2b4f23537b76a72a6f5d32436b4156528
Kenny DiBattista <KenDB3@users.noreply.github.com>
2016-09-02 15:46:33 -0400

    Create bitcoin.js
    Nothing to see yet, just an explanation of what the app does
    and where the idea came from.

---
+++ bitcoin.js
@@ -0,0 +1,21 @@
+/*KD3 Doors presents SyncBTC - A Bitcoin Exchange Rate external program (door) 
+written in javascript for Synchronet BBS and ANSI capable terminals.
+
+SyncBTC is based off of something cool I saw at BitSunrise.com (aka: Black Country Rock/Bit Sunrise). 
+All of my code was original, but the look and feel of the app is based on the BCR original program: 
+BCR Coins v1.0 - Real-time Bitcoin Price Module for Wildcat! and Winserver Bulletin Board Systems
+I highly recommend you log into telnet://BitSunrise.com and check it out. 
+You can see the conversation that started this project here:
+https://www.reddit.com/r/bbs/comments/4owbrt/bcr_coins_an_ansi_bitcoin_price_chart_generator/
+
+All of the data is pulled from Coinbase.com and the queries used do not need any API Key.
+So big thanks to Coinbase.com!
+
+Also, thanks to http://www.xe.com/symbols.php for help with some of the currency symbols.
+
+I didn't know if I could get this door working exactly the way I wanted, so I held off on 
+developing it with GitHub. But, lo and behold, I got it working and I am pretty happy with
+the results. I saved various stages of the work along the way, and so I am going to push 
+my various snapshots of the work in Git to track some of the development. But the comments
+on the commits are going to be little to nonexistant. Sorry about that.
+*/
